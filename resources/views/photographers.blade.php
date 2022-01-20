0509415a8b6a31f31853ce2b39cfd5fc0fa4478d
salvosanzone <salvosansone.ss@gmail.com>
2022-01-20 13:51:38 +0100

    first commit

---
+++ resources/views/photographers.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Photographers</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+</head>
+<body>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">MAGNUM PHOTOS</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+          <a class="nav-link active" aria-current="page" href="{{route('home')}}">Home</a>
+          
+      </div>
+    </div>
+  </nav>
+  <div class="container d-flex flex-column align-items-center my-2">
+    <h2>MAGNUM PHOTOGRAPHERS</h2>
+    <h6>Magnum Photos is a co-operative owned and run by its member photographers, who undergo a rigorous process of self-selection in order to become full members. The photographers meet once a year, during the last weekend in June, in New York, Paris or London, to discuss Magnum’s affairs.</h6>
+  </div>
+</body>
+</html>
